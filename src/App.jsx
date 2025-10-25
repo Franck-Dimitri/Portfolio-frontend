465f0a83a0e8b720ed956e774ca31170bb4157b6
Franck-Dimitri <franckdimitrio009@gmail.com>
2025-10-25 14:33:46 +0200

    Initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { CalendarMinus } from "lucide-react/"
+
+export default function App() {
+  return (
+    <h1 className="text-3xl mt-20 font-bold underline text-blue-500">
+      Hello world!
+      <CalendarMinus name="usersearchicon" size={48} />
+    </h1>
+  )
+}
